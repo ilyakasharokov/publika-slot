f8ca156c97644a3f8c37bff10eaa8abad473593c
ikasharokov <ikasharokov>
2023-03-31 15:35:31 +0300

    work

--- js/main.js
+++ js/main.js
@@ -1,28 +1,32 @@
 import 'regenerator-runtime/runtime';
+import image from '/img/arm.png';
+import balaklava from '/img/balaklava.jpg';
+import pblk from '/img/pblk.png';
+import trash from '/img/trash.png';
+import quest from '/img/quest.png';
 
-window.addEventListener('load', function () {
+(function () {
+  //const API_HOST = "http://mystery.publikagaultier.com/api/";
+  const API_HOST = "http://localhost:8090/"
+  let finish = false;
+  let email = "not set";
+
   const items = [
-    '🍭',
-    '❌',
-    '⛄️',
-    '🦄',
-    '🍌',
-    '💩',
-    '👻',
-    '😻',
-    '💵',
-    '🤡',
-    '🦖',
-    '🍎',
-    '😂',
-    '🖕',
+      '<img src="' + balaklava + '">',
+    '<img src="' + pblk + '">', '<img src="' + trash + '">',
+    '<img src="' + balaklava + '">',
+    '<img src="' + pblk + '">', '<img src="' + trash + '">', '<img src="' + quest + '">',
   ];
   const doors = document.querySelectorAll('.door');
+  let isSpinning = false;
 
-  document.querySelector('#spinner').addEventListener('click', spin);
-  document.querySelector('#reseter').addEventListener('click', init);
+  document.querySelector('#slot').addEventListener('click', spin);
+  document.querySelector('#slot').addEventListener('touchmove', spin);
+  document.querySelector('#form').addEventListener('submit', submitEmail);
+  /*document.querySelector('#reseter').addEventListener('click', init);*/
 
-  function init(firstInit = true, groups = 1, duration = 1) {
+  function init(firstInit = true, groups = 1, duration = 1, win = false) {
+    const bones = [];
     for (const door of doors) {
       if (firstInit) {
         door.dataset.spinned = '0';
@@ -32,14 +36,19 @@ window.addEventListener('load', function () {
 
       const boxes = door.querySelector('.boxes');
       const boxesClone = boxes.cloneNode(false);
-      const pool = ['❓'];
+      const pool = [''];
 
       if (!firstInit) {
         const arr = [];
         for (let n = 0; n < (groups > 0 ? groups : 1); n++) {
           arr.push(...items);
         }
-        pool.push(...shuffle(arr));
+        if(win){
+          pool.push(...arr);
+        }else {
+          pool.push(...shuffle(arr));
+          bones.push(pool[pool.length - 1]);
+        }
 
         boxesClone.addEventListener(
           'transitionstart',
@@ -69,23 +78,43 @@ window.addEventListener('load', function () {
         box.classList.add('box');
         box.style.width = door.clientWidth + 'px';
         box.style.height = door.clientHeight + 'px';
-        box.textContent = pool[i];
+        box.innerHTML = pool[i];
         boxesClone.appendChild(box);
       }
       boxesClone.style.transitionDuration = `${duration > 0 ? duration : 1}s`;
       boxesClone.style.transform = `translateY(-${door.clientHeight * (pool.length - 1)}px)`;
       door.replaceChild(boxesClone, boxes);
     }
+    if(!win && bones[0] === bones[1] === bones[2]){
+      init(firstInit,groups,duration, win);
+      return
+    }
   }
 
   async function spin() {
-    init(false, 1, 1);
+    if(isSpinning || finish){
+      return;
+    }
+    document.querySelector('.arm').classList.add("active");
+    isSpinning = true;
+    let response = await fetch( API_HOST + "lottery", { method: "POST", body:  JSON.stringify({email: email })})
+    response = await response.json()
+
+    init(false, 3, 1, !!response.code);
 
     for (const door of doors) {
       const boxes = door.querySelector('.boxes');
       const duration = parseInt(boxes.style.transitionDuration);
       boxes.style.transform = 'translateY(0)';
       await new Promise((resolve) => setTimeout(resolve, duration * 100));
+    }
+    setTimeout(()=>{
+      isSpinning = false;
+    },1000);
+    document.querySelector('.arm').classList.remove("active");
+    if(response.code){
+      finish = true;
+      onWin(response.code);
     }
   }
 
@@ -98,6 +127,35 @@ window.addEventListener('load', function () {
     return arr;
   }
 
+  async function submitEmail(e) {
+    e.preventDefault();
+    document.querySelector('#email').classList.remove("error");
+    if(validateEmail(document.querySelector('#email').value)) {
+      email = document.querySelector('#email').value;
+      document.querySelector('#form').remove();
+      document.querySelector('#overlay').classList.remove('visible');
+      let response = await fetch(API_HOST + "email", { method: "POST", body:  JSON.stringify({email: email })})
+      await response.text()
+    }else{
+      document.querySelector('#email').classList.add("error");
+    }
+  }
+
+  function onWin(code){
+    setTimeout(function () {
+      document.body.classList.add("win");
+      document.getElementById("mystery").textContent = "PROMOCODE: \n" + code;
+    }, 1000);
+  }
+
+  function validateEmail(email){
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
   init();
-});
+})();
 
